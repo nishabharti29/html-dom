498f79ac9df745c31dbd81d1d700dccfc01584ad
Nisha <nishabps.7@gmail.com>
2025-04-19 15:34:32 +0530

    Add files via upload

---
+++ chapter 5/2.js
@@ -0,0 +1,5 @@
+console.log(link.getAttribute('href')); 
+console.log(img.getAttribute('src'));   
+console.log(input.getAttribute('value'));
+console.log(input.getAttribute('disabled'));
+console.log(link.getAttribute('target'));
---
+++ chapter 5/3.js
@@ -0,0 +1,4 @@
+link.setAttribute('href', 'another-page.html');
+img.setAttribute('alt', 'Another description');
+input.setAttribute('maxlength', '10'); 
+input.setAttribute('disabled', ''); 
